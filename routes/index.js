802a3040140c63e3076862db5cb8d3f1a5af13d1
ahmedbouhmid <ahmed.bouhmid94@gmail.com>
2016-07-12 17:39:16 +0100

    made some changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('Main.html',{"root": '/home/ahmed/Mean_Stack_projects/Rest_Countries/public'})
+  res.sendFile('Main.html',{"root": 'public'})
 });
 
 module.exports = router;
